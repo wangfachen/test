2a1400cebb3dda15115d3e8e4b750a80103fb8aa
wangfachen <771510470@qq.com>
2021-05-18 15:22:28 +0800

    add a new file

---
+++ hello.c
@@ -0,0 +1,21 @@
+/*================================================================
+*   Copyright (C) 2021 hqyj Ltd. All rights reserved.
+*   
+*   文件名称：hello.c
+*   创 建 者：wfc
+*   创建日期：2021年05月18日
+*   描    述：
+*
+================================================================*/
+
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+	
+	printf("hello world\n");
+	return 0;
+}
+
